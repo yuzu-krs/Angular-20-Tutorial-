c761f76fc54e7a1df4b8ae9667f09d1713a408f0
yuzu-krs <yuzuto.poi@gmail.com>
2025-11-30 21:09:40 +0900

    ライフサイクルイベント&パイプ

--- src/app/att-directive/att-directive.ts
+++ src/app/att-directive/att-directive.ts
@@ -1,10 +1,36 @@
-import { Component, signal } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  Component,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
 //直接のパスで呼び出すのは禁止。APIからインポートすること
-import { NgClass, NgStyle } from '@angular/common';
+import {
+  DatePipe,
+  JsonPipe,
+  LowerCasePipe,
+  NgClass,
+  NgStyle,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
 
 @Component({
   selector: 'app-att-directive',
-  imports: [NgClass, NgStyle],
+  imports: [
+    NgClass,
+    NgStyle,
+    UpperCasePipe,
+    LowerCasePipe,
+    TitleCasePipe,
+    SlicePipe,
+    JsonPipe,
+    DatePipe,
+  ],
   templateUrl: './att-directive.html',
   styleUrl: './att-directive.css',
   //standaloneとはそのコンポーネント単体で動作することを意味する
@@ -15,10 +41,65 @@ import { NgClass, NgStyle } from '@angular/common';
   //  imports: [CommonModule],       // このモジュールが使える機能
   //  bootstrap: [AppComponent]      // 最初に動くコンポーネント
 })
-export class AttDirective {
+export class AttDirective
+  implements
+    OnInit,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewChecked,
+    AfterViewChecked,
+    OnDestroy
+{
   div1ClassName = signal<string>('');
 
+  firstName: string = 'Yuzu!';
+  courseName: string = 'Angular 20 tutorial';
+
+  rollNoList: number[] = [1, 2, 3, 4, 5];
+
+  studentObj: { [key: string]: any } = {
+    name: 'Yuzu',
+    city: 'Tokyo',
+    state: 'Japan',
+  };
+
+  currentDate: Date = new Date();
+
   isDiv2Green: boolean = false;
+
+  //最初に呼ばれる
+  constructor() {
+    console.log('constructor AttDirective');
+  }
+
+  //コンストラクタの次に呼ばれる
+  ngOnInit(): void {
+    console.log('ngOnInit AttDirective');
+    //APIコールをトリガーするため
+  }
+
+  //コンテンツの初期化が完了した後に呼ばれる
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked AttDirective');
+  }
+
+  //ビューの初期化が完了した後に呼ばれる
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit AttDirective');
+    //子コンポーネントの初期化が完了した後に呼ばれる
+  }
+
+  //ビューの変更検出が完了した後に呼ばれる
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked AttDirective');
+  }
+
+  //コンポーネントが破棄される直前に呼ばれる(他のコンポーネントに切り替わるなど)
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy AttDirective');
+
+    //サブスクリプションの解除などを行う
+  }
 
   toggleDiv2Color() {
     this.isDiv2Green = !this.isDiv2Green;
